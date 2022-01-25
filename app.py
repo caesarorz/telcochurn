80df2b535f70a69d9d1f7b3ddfad5018740255a4
Cesar Orozco <caesar.orz@gmail.com>
2022-01-25 11:47:27 -0600

    Add text, generate table for prediction and model preview results

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from distutils.log import debug
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pandas as pd
 from catboost import CatBoostClassifier
 
@@ -10,67 +9,67 @@ app.config['SECRET_KEY'] = 'mysecretkey'
 
 
 
-list_prediction = ['SeniorCitizen', 'tenure', 'MonthlyCharges', 'TotalCharges', 'gender_Male', 'Partner_Yes', 'Dependents_Yes', 
-'PhoneService_Yes', 'MultipleLines_No phone service', 'MultipleLines_Yes', 'InternetService_Fiber optic', 'InternetService_No', 
-'OnlineSecurity_No internet service', 'OnlineSecurity_Yes', 'OnlineBackup_No internet service', 'OnlineBackup_Yes', 'DeviceProtection_No internet service', 
-'DeviceProtection_Yes', 'TechSupport_No internet service', 'TechSupport_Yes', 'StreamingTV_No internet service', 'StreamingTV_Yes', 
-'StreamingMovies_No internet service', 'StreamingMovies_Yes', 'Contract_One year', 'Contract_Two year', 'PaperlessBilling_Yes', 
-'PaymentMethod_Credit card (automatic)', 'PaymentMethod_Electronic check', 'PaymentMethod_Mailed check', 'Tenure Cohort_12-24 months', 
-'Tenure Cohort_24-48 months', 'Tenure Cohort_Over 48 months']
-
-
-def generate_data(data):
-    thedict = {}
-    for i,j in zip(list_prediction, data):
-        thedict[i] = j
-    return thedict
+list_names = ['SeniorCitizen', 'tenure', 'MonthlyCharges', 'TotalCharges', 'gender_Male', 'Partner_Yes', 'Dependents_Yes', 
+                    'PhoneService_Yes', 'MultipleLines_No phone service', 'MultipleLines_Yes', 'InternetService_Fiber optic', 'InternetService_No', 
+                    'OnlineSecurity_No internet service', 'OnlineSecurity_Yes', 'OnlineBackup_No internet service', 'OnlineBackup_Yes', 'DeviceProtection_No internet service', 
+                    'DeviceProtection_Yes', 'TechSupport_No internet service', 'TechSupport_Yes', 'StreamingTV_No internet service', 'StreamingTV_Yes', 
+                    'StreamingMovies_No internet service', 'StreamingMovies_Yes', 'Contract_One year', 'Contract_Two year', 'PaperlessBilling_Yes', 
+                    'PaymentMethod_Credit card (automatic)', 'PaymentMethod_Electronic check', 'PaymentMethod_Mailed check', 'Tenure Cohort_12-24 months', 
+                    'Tenure Cohort_24-48 months', 'Tenure Cohort_Over 48 months']
 
 
 def process_data(data):
-    prediction = [0]*len(list_prediction)
+    """
+    get the data from the client and transform information into
+    a format that the catboost prediction can understand.
+
+    input: data as a dictionary, list_names
+    output: data in predict format (see format for prediction) 
+    """
+    prediction = [0]*len(list_names)
     for i, j in data.items():
-        for x in list_prediction:
+        for x in list_names:
             if i.lower() in x.lower():
                 if j == x:
-                    theindex = list_prediction.index(j)
+                    theindex = list_names.index(j)
                     prediction[theindex] = 1
                 if j.isnumeric():
                     if 'tenure' in i:
-                        theindex = list_prediction.index('tenure')
+                        theindex = list_names.index('tenure')
                         prediction[theindex] = j
                         if int(j) >= 12 and int(j) < 24:
-                            theindex = list_prediction.index('Tenure Cohort_12-24 months')
+                            theindex = list_names.index('Tenure Cohort_12-24 months')
                             prediction[theindex] = 1
                             
                         elif int(j) >= 24 and int(j) < 48:
-                            theindex = list_prediction.index('Tenure Cohort_24-48 months')
+                            theindex = list_names.index('Tenure Cohort_24-48 months')
                             prediction[theindex] = 1
                             
                         elif int(j) >= 48:
-                            theindex = list_prediction.index('Tenure Cohort_Over 48 months')
+                            theindex = list_names.index('Tenure Cohort_Over 48 months')
                             prediction[theindex] = 1     
                     else:
-                        theindex = list_prediction.index(i)
-                        prediction[theindex] = j
-                        
+                        theindex = list_names.index(i)
+                        prediction[theindex] = j 
     return prediction
 
 
-
-
-@app.route('/', methods=['get', 'post'])
+@app.route('/', methods=['get'])
 def index():
-    clf2 = CatBoostClassifier()
-    clf2.load_model(fname="catboost.model", format="cbm")
-    
-    prediction = []
-    if request.method == 'POST':
-        prediction = process_data(request.form)
-        show_results = generate_data(prediction)
-        prediction = clf2.predict(prediction)
-        return render_template('index.html', prediction=prediction, results=show_results)
     return render_template('index.html')
 
+
+@app.route('/prediction', methods=['post'])
+def predict():
+    prediction = []
+    if request.method == 'POST':
+        json_ = request.json
+        clf2 = CatBoostClassifier()
+        clf2.load_model(fname="catboost.model", format="cbm")
+        prediction = process_data(json_)
+        prediction = clf2.predict(prediction)
+        return jsonify({'prediction': str(prediction)}), 200
+    return jsonify({'message', 'Unknown error ocurred.'}), 500
 
 
 if __name__ == '__main__':
--- static/script.js
+++ static/script.js
@@ -25,36 +25,109 @@
   output3.innerHTML = slider3.value;
   
   slider3.oninput = function() {
-    console.log(this.value)
     output3.innerHTML = this.value;
   }
-
 
   var slider4 = document.getElementById("customRange4");
   var output4 = document.getElementById("customRange4Value");
   output4.innerHTML = slider4.value;
   
   slider4.oninput = function() {
-    console.log(this.value)
     output4.innerHTML = this.value;
   }
-
 
   var slider5 = document.getElementById("customRange5");
   var output5 = document.getElementById("customRange5Value");
   output5.innerHTML = slider5.value;
   
   slider5.oninput = function() {
-    console.log(this.value)
     output5.innerHTML = this.value;
   }
 
-  document.getElementById('submit-prediction').addEventListener('click', function () {
-    var formEl = document.forms.churn_form;
-    var formData = new FormData(formEl);
-    console.log(formData)
-    // var name = formData.get('name');
-  })
+document.querySelector('.btn-close').addEventListener('click', (e) => {
+  document.getElementById('alert-result').classList.toggle('hide-element')
+})
+
+
+const formatValuesTable = (value) => {
+  if (value === 'SeniorCitizen') {
+    return "Yes"
+  }
+  start = value.indexOf("_");
+  new_value = value.slice(start+1, value.length);
+  return new_value
+}
+
+const rendertablePrediction = (data) => {
+  table_body = document.querySelector(".fill-data")
+  data = JSON.parse(data)
+  for (const [key, value] of Object.entries(data)) {
+      var tr = document.createElement('tr');   
+      var td1 = document.createElement('td');
+      var td2 = document.createElement('td');
+      var text1 = document.createTextNode(`${key}`);
+      var text2 = document.createTextNode(`${formatValuesTable(value)}`);
+      td1.appendChild(text1);
+      td2.appendChild(text2);
+      tr.appendChild(td1);
+      tr.appendChild(td2);
+      table_body.appendChild(tr);
+  }
+}
+
+const renderResult = (data) => {
+  if (data.prediction === 'No' || data.prediction === 'Yes'){
+    document.getElementById("prediction-result").innerHTML = `Model Result: ${data.prediction}`
+    document.getElementById('alert-result').classList.toggle("hide-element")
+    // document.getElementById('alert-result').classList.toggle("show-element")
+  }
+  else {
+    document.getElementById("prediction-result").innerHTML = `Error in prediction, we are working to fix the issue.`
+  }
+}
+
+
+const Form = document.getElementById("churn_form");
+
+Form.addEventListener("submit", handleFormSubmit);
+ async function handleFormSubmit(event) {
+	event.preventDefault();
+	const form = event.currentTarget;
+	const url = form.action;
+	try {
+		const formData = new FormData(form);
+		const responseData = await postFormDataAsJson({ url, formData });
+    renderResult(responseData)
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+
+ async function postFormDataAsJson({ url, formData }) {
+	const plainFormData = Object.fromEntries(formData.entries());
+	const formDataJsonString = JSON.stringify(plainFormData);
+
+	const fetchOptions = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Accept": "application/json"
+		},
+		body: formDataJsonString,
+	};
+
+	const response = await fetch(url, fetchOptions);
+
+
+	if (!response.ok) {
+		const errorMessage = await response.text();
+		throw new Error(errorMessage);
+	}
+
+  rendertablePrediction(formDataJsonString)
+	return response.json();
+}
 
 
 
